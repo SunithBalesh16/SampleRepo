4cdf5d43ca46560737d12ddf9781b77a4b83db42
Sunith <sunith.balesh@gmail.com>
2017-03-05 01:20:26 +0530

    changes

--- LiveConnect/SliderViewController.swift
+++ LiveConnect/SliderViewController.swift
@@ -12,7 +12,7 @@ class SliderViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("sunith")
+        print("sunit")
         // Do any additional setup after loading the view.
     }
 

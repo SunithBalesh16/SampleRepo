4bd40b3dc624c64c248e697efb1767d17833af51
sunithc2 <sunith.b@c2info.com>
2017-03-06 00:55:29 +0530

    Changed to lohit

--- LiveConnect/SliderViewController.swift
+++ LiveConnect/SliderViewController.swift
@@ -12,7 +12,7 @@ class SliderViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("sunith")
+        print("lohit")
         // Do any additional setup after loading the view.
     }
 
